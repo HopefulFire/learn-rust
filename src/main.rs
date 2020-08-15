f7a858aa32b673aaa752f6b52da2fa101377e783
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 19:14:51 -0400

    remove scope and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-fn scope_and_shadowing()
-{
-    let a = 123;
-    {
-        let b = 456;
-        println!("inside {:?}", b);
-        let a = 777;
-        println!("inside {:?}", a);
-    }
-    println!("outside {:?}", a);
-    // println!("{:?}", b);
-}
+
 
 fn main()
 {
-    scope_and_shadowing()
 }
