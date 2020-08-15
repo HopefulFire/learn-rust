74d03f45101f68df9c2424e1045c850799e977f9
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 17:39:53 -0400

    primitive types in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    let a:u8 = 123;
+    println!("a = {:?}", a);
+    //cannot a = 128;
+    let mut b:i8 = 0;
+    println!("b = {:?}", b);
+    b = 42;
+    println!("b = {:?}", b);
+
+    let mut c = 123456789;
+    println!("c = {:?}, size = {:?}", c, mem::size_of_val(&c));
+    c = -1;
+    println!("c = {:?}, size = {:?}", c, mem::size_of_val(&c));
+
+    let z:isize = 123;
+    let size_of_z = mem::size_of_val(&z);
+    println!("z = {}, takes up {} bytes, {}-bit OS", z, size_of_z, size_of_z * 8);
+
+    let d:char = 'x';
+    println!("d = {}", d);
+
+    let g = false;
+    println!("g = {}", g);
+
+    let f = 4>0;
+    println!("f = {}", f);
 }
