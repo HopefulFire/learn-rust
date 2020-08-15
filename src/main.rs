a7adbbb9bbd2dfcff12e22a78c9ef226531c75aa
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 19:59:40 -0400

    remove previous lesson

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
 
-fn if_statement()
-{
-    let temp = 15;
-    if temp > 30
-    {
-        println!("Really hot outside!");
-    }
-    else if temp < 10
-    {
-        println!("Baby it's cold outside!");
-    }
-    else
-    {
-        println!("Temperature is ok.");
-    }
-    let day = if temp > 20 {"sunny"} else {"cloudy"};
-    println!("today is {}", day);
-    println!("it is {}", if temp > 20 {"hot"} else if temp < 10 {"cold"} else {"normal"});
-    // more stuff
-}
-
 fn main()
 {
-    if_statement();
+
 }
