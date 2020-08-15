2378d707a2b98be6ddffb99d2ebba2de6c19363e
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 19:14:11 -0400

    scope and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+fn scope_and_shadowing()
+{
+    let a = 123;
+    {
+        let b = 456;
+        println!("inside {:?}", b);
+        let a = 777;
+        println!("inside {:?}", a);
+    }
+    println!("outside {:?}", a);
+    // println!("{:?}", b);
+}
+
 fn main()
 {
-    
+    scope_and_shadowing()
 }
