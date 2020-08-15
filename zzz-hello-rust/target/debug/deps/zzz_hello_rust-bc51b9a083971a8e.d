23ee5311e5c13c142855b8b0d798a0d69e48dbdd
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 15:37:57 -0400

    hello world!

---
+++ zzz-hello-rust/target/debug/deps/zzz_hello_rust-bc51b9a083971a8e.d
@@ -0,0 +1,5 @@
+/home/jadelynnmasker/Documents/repos/learn-rust/zzz-hello-rust/target/debug/deps/zzz_hello_rust-bc51b9a083971a8e: src/main.rs
+
+/home/jadelynnmasker/Documents/repos/learn-rust/zzz-hello-rust/target/debug/deps/zzz_hello_rust-bc51b9a083971a8e.d: src/main.rs
+
+src/main.rs:
---
+++ zzz-hello-rust/target/debug/zzz-hello-rust.d
@@ -0,0 +1,1 @@
+/home/jadelynnmasker/Documents/repos/learn-rust/zzz-hello-rust/target/debug/zzz-hello-rust: /home/jadelynnmasker/Documents/repos/learn-rust/zzz-hello-rust/src/main.rs
