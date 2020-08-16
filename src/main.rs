adce53b2ff67a4adefe166cc1690be21c933f0b0
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 20:50:17 -0400

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+fn for_loop()
+{
+    for x in 1..=10
+    {
+        if x == 3 { continue; }
+        if x == 8 { break; }
+        println!("x = {}", x);
+    }
+    for (pos, y) in (30..=40).enumerate()
+    {
+        println!("{}: {}", pos, y);
+    }
+}
 
 fn main()
 {
-
+    for_loop();
 }
