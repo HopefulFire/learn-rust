a3d15e52743cc0ac27b621b572219bb7a2c3072b
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-16 18:38:11 -0400

    hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-
+use std::collections::HashMap;
 fn main()
 {
+    let mut shapes = HashMap::new();
+    shapes.insert(String::from("triangle"), 3);
+    shapes.insert(String::from("quadrilateral"), 4);
+    shapes.insert(String::from("heptagon"), 5);
+    shapes.insert(String::from("hexagon"), 6);
+
+    shapes.insert(String::from("quadrilateral"), 5);
+
+    println!("{:?}", shapes);
+
+    shapes.entry("circle".into()).or_insert(1);
+    {
+        let actual = shapes.entry("circle".into()).or_insert(2);
+        *actual = 0;
+        println!("{:?}", shapes);
+    }
 }
