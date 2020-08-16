56ffaa31365c437c3e7dc8a98297939c49031bf3
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-16 14:57:28 -0400

    generics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+
+struct Point<T>
+{
+    x: T,
+    y: T
+}
+
+struct Line<T>
+{
+    start: Point<T>,
+    end: Point<T>
+}
+
+
+fn generics()
+{
+    let a:Point<f64> = Point{x:0.0, y:0.0};
+    let b:Point<f64> = Point{x:4.1, y:1.2};
+    let myline = Line{start:a, end:b};
+}
+
 fn main()
 {
-
+    generics()
 }
