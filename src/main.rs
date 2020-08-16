382b85a71896dad53e5b62085445e943a870c8c4
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-16 12:50:25 -0400

    tuples

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
 
+fn sum_and_product(x:i32, y:i32) -> (i32, i32)
+{
+    return (x+y, x*y)
+}
+
+fn tuples()
+{
+    let x = 3;
+    let y = 4;
+    let sp = sum_and_product(x,y);
+    println!("sp = {:?}", sp);
+    println!("{0} + {1} = {2}, {0} * {1} = {3}", x, y, sp.0, sp.1);
+
+    //destructuring
+    let (a,b) = sp;
+    println!("a = {}, b = {}", a, b);
+    let sp2 = sum_and_product(4,7);
+    let combined = (sp, sp2);
+    println!("{:?}", combined);
+    println!("last element");
+}
 
 fn main()
 {
+    tuples()
 }
