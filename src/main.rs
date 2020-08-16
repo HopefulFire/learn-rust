4c562f5efe3cfda1049b8109dcc68e90163620d2
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-16 07:38:15 -0400

    remove last lesson

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
 fn main()
 {
-    let x = 3.0;
-    let y = 2.0;
 
-    let result = if y != 0.0 {Some(x/y)} else {None};
-    match result
-    {
-        Some(z) => println!("{}/{}={}", x, y, z),
-        None => println!("Cannot divide by zero"),
-    }
-
-    if let Some(z) = result {println!("result = {}", z)};
 }
