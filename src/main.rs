707604f34505e4f7a24b310303833f99280071b1
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 20:39:07 -0400

    remove previous lesson

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,5 @@
 
-fn while_and_loop()
-{
-    let mut x:u64 = 1;
-    while x < 1000
-    {
-        x *= 2;
-        if x == 64
-        {
-            continue;
-        }
-        println!("{}", x);
-    }
-    let mut y:u64 = 0;
-    loop
-    {
-        y += 1;
-        println!("y = {}", y);
-
-        if y == 1<<10
-        {
-            break;
-        }
-    }
-}
-
 fn main()
 {
-    while_and_loop();
+
 }
