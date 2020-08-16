9a757ed6d86c65b0f4c32765525b2af23596c945
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 21:00:05 -0400

    match block

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,20 @@
 
 fn main()
 {
+    let country_code = 44;
+
+    let country = match country_code {
+        44 => "UK",
+        46 => "Sweeden",
+        7 => "Russia",
+        1..=1000 => "Unknown",
+        _ => "Invalid"
+    };
+
+    println!("The country with code {} is {}", country_code, country);
+    let x = false;
+    let s = match x {
+        true => "yes",
+        false => "no"
+    };
 }
