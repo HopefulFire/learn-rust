03ad659361c91e8d5a3fad86f76de58200341282
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-16 07:37:56 -0400

    while let and if let

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main()
 {
+    let x = 3.0;
+    let y = 2.0;
+
+    let result = if y != 0.0 {Some(x/y)} else {None};
+    match result
+    {
+        Some(z) => println!("{}/{}={}", x, y, z),
+        None => println!("Cannot divide by zero"),
+    }
+
+    if let Some(z) = result {println!("result = {}", z)};
 }
