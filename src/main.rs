b130c04de464e6b4e88f1f4d06d238a544b00c35
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-15 21:40:27 -0400

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
+struct Point
+{
+    x: f64,
+    y: f64
+}
+
+struct Line
+{
+    start: Point,
+    end: Point
+}
+
+fn structures()
+{
+    let p1 = Point{x: 3.0, y: 4.0};
+    println!("point p is at {}, {}", p1.x, p1.y);
+
+    let p2 = Point{x: 5.0, y:10.0};
+
+    let my_line = Line{start: p1, end: p2};
+
+    println!("{}", my_line.start.x);
+
+}
 
 fn main()
 {
+    structures()
 }
