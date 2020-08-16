77e072838ac1e4901bec2ec5b6c7fd9767e91ee4
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-16 07:10:47 -0400

    unions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+union IntOrFloat
+{
+    i: i64,
+    f: f64
+}
+
+fn process_value(iof:IntOrFloat)
+{
+    unsafe
+    {
+        match iof
+        {
+            IntOrFloat{i: 42} => {
+                println!("meaning of life");
+            }
+            IntOrFloat{f} => {
+                println!("value = {}", f);
+            }
+        }
+    }
+}
+
 fn main()
 {
+    let mut iof = IntOrFloat{ f:1.24 };
+    iof.i = 234;
+
+    let value = unsafe { iof.i };
+    println!("{:?}", value);
+    process_value(IntOrFloat{i:5})
 }
