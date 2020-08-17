c82d4abe3e1c9d31f63cd5a2ea23c7cb8a2c216c
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-17 01:26:41 -0400

    remove previous lesson

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,4 @@
-fn print_value(x:i32)
-{
-    println!("{}", x);
-}
-
-fn increase(x:&mut i32)
-{
-    *x += 1;
-}
-
-fn product(x:i32, y:i32) -> i32
-{
-    return x * y
-}
-
-fn functions()
-{
-    print_value(33);
-    let mut z:i32 = 1;
-    increase(&mut z);
-    println!("{}", z);
-
-    let a = 3;
-    let b = 5;
-    let p = product(a, b);
-    println!("{}", p);
-}
 
 fn main()
 {
-    functions()
 }
