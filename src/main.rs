1d258015f13db40c333945c57c52b855949321d3
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-16 20:37:27 -0400

    remove previous lesson

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,5 @@
 
 fn main()
 {
-    let name = "Jade";
-    let greeting = format!("hi, I'm {} nice to meet you", name);
-    println!("{}", greeting);
 
-    let run = "run";
-    let forrest = "forrest";
-    let rfr = format!("{0}, {1}, {0}", run, forrest);
-
-    println!("{}", rfr);
-
-    let info = format!(
-        "the name's {last}. {first} {last}.",
-        first = "James",
-        last = "Bond"
-    );
-    println!("{}", info);
-
-    let mixed = format!(
-        "{1} {} {0} {} {data}",
-        "alpha",
-        "beta",
-        data = "delta"
-    );
-    println!("{}", mixed);
 }
