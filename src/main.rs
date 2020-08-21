8926ecc428ea50df79be85628b0c3e54112bb706
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-21 17:19:38 -0400

    minesweeper game (broken)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod minesweeper;
+
 fn main()
 {
+    let mut game = minesweeper::Minesweeper::new(10, 10, 10);
+    game.display_cleared()
 }
---
+++ src/minesweeper.rs
@@ -0,0 +1,182 @@
+use rand::prelude::*;
+
+#[derive(Clone)]
+enum State
+{
+    Hidden,
+    Near(usize),
+}
+
+#[derive(Clone)]
+enum Tile
+{
+    Clear(State),
+    Mined,
+    Flagged(Box<Tile>)
+}
+
+pub struct Minesweeper
+{
+    board: Vec<Vec<Tile>>
+}
+
+impl Minesweeper
+{
+    pub fn new(xdim:usize, ydim:usize, mines:usize) -> Minesweeper
+    {
+        let mut game = Minesweeper{board: Vec::new()};
+        game.generate(xdim, ydim, mines);
+        game
+    }
+
+    pub fn touch_mine(&mut self, x:usize, y:usize) -> bool
+    {
+        match self.board[x][y]
+        {
+            Tile::Clear(State::Hidden) => {
+                let mines = self.find_mines_around(x, y);
+                self.board[x][y] = Tile::Clear(State::Near(mines));
+                false
+            },
+            Tile::Clear(_) => {false},
+            Tile::Flagged(_) => {false},
+            Tile::Mined => {true},
+        }
+    }
+
+    pub fn toggle_flag(&mut self, x:usize, y:usize) -> bool
+    {
+        match &self.board[x][y]
+        {
+            Tile::Flagged(tile) => {
+                self.board[x][y] = *tile.clone();
+                true
+            },
+            Tile::Mined => {
+                self.board[x][y] = Tile::Flagged(Box::new(Tile::Mined));
+                true
+            },
+            Tile::Clear(State::Hidden) => {
+                self.board[x][y] = Tile::Flagged(Box::new(Tile::Clear(State::Hidden)));
+                true
+            }
+            _ => {false},
+        }
+    }
+
+    pub fn display(&self)
+    {
+        for y in 0..self.board[0].len()
+        {
+            for x in 0..self.board.len()
+            {
+                match self.board[x][y]
+                {
+                    Tile::Clear(State::Hidden) => {
+                        print!("~");
+                    },
+                    Tile::Clear(State::Near(mines)) => {
+                        print!("{}", mines);
+                    },
+                    Tile::Mined => {
+                        print!("~");
+                    },
+                    Tile::Flagged(_) => {
+                        print!("F");
+                    },
+                }
+            }
+            println!("");
+        }
+    }
+
+    pub fn display_cleared(&mut self)
+    {
+        for x in 0..self.board.len()
+        {
+            for y in 0..self.board[0].len()
+            {
+                self.touch_mine(x, y);
+            }
+        }
+        self.display();
+    }
+
+    fn generate(&mut self, xdim:usize, ydim:usize, mut mines:usize)
+    {
+        let mut tiles = Vec::new();
+        for _ in 0..(xdim * ydim)
+        {
+            if mines == 0
+            {
+                tiles.push(Tile::Clear(State::Hidden));
+            }
+            else
+            {
+                tiles.push(Tile::Mined);
+                mines -= 1;
+            }
+        }
+        let mut rng = rand::thread_rng();
+        tiles.shuffle(&mut rng);
+        for _x in 0..xdim
+        {
+            let mut column = Vec::new();
+            for _y in 0..ydim
+            {
+                match tiles.pop()
+                {
+                    Some(tile) => {
+                        column.push(tile)
+                    },
+                    None => {
+                        column.push(Tile::Mined);
+                        println!("Error in generation!");
+                    },
+                }
+            }
+            self.board.push(column);
+        }
+    }
+
+
+    fn find_mines_around(&self, x:usize, y:usize) -> usize
+    {
+        let mut mines:usize = 0;
+        for dx in -1..2
+        {
+            if x as isize + dx == -1 || x as isize + dx == self.board.len() as isize
+            {
+                continue;
+            }
+            for dy in -1..2
+            {
+                if y as isize + dy == -1 || y as isize + dy == self.board[0].len() as isize
+                {
+                    continue;
+                }
+                if self.is_mined((x as isize + dx) as usize, (y as isize + dx) as usize)
+                {
+                    mines += 1;
+                }
+            }
+        }
+        mines
+    }
+
+    fn is_mined(&self, x:usize, y:usize) -> bool
+    {
+        match &self.board[x][y]
+        {
+            Tile::Mined => {true},
+            Tile::Flagged(tile) => {
+                match **tile
+                {
+                    Tile::Mined => {true},
+                    _ => {false},
+                }
+            },
+            _ => {false},
+        }
+    }
+}
