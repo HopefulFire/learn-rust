42266a3c99c9fcc85ed1b9123eee5039066fba30
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-26 18:20:05 -0400

    removed minesweeper

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-mod cli;
 
 fn main()
 {
-    match cli::CommandLineInterface::new()
-    {
-        Some(mut run) => {
-            run.event_loop()
-        },
-        None => {
-            println!("Error!");
-        },
-    }
+
 }
